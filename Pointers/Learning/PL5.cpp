f24ed6d34ab7ba88166ee8ea2ee9ac29f5c3872d
Nehal <nehalgarg37@gmail.com>
2023-03-15 22:08:12 +0530

    commit PL5

--- Pointers/Learning/PL5.cpp
+++ Pointers/Learning/PL5.cpp
@@ -1,7 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
+    int *ptr; // Wild Pointer
+   // cout << ptr <<endl << *ptr <<endl;
 
-return 0;
+    int *ptf = NULL; // NULL Pointer
+    int *p = 0;// NULL Pointer "Since 0 is mostly a special reserved memory address"
+    int *ppp ='\0';// NULL Pointer
+
+   // cout << ptf <<endl << *ptf <<endl;
+
+    float i = 10.2;
+    void *ptr1 = &i;
+
+    int *l = (int *)ptr1; // Void pointer can be typecase but cannot be dereferenced.
+
+    cout << *l;
+
+
+    return 0;
 }
