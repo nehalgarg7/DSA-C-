d83c44f08199ee0ba9d09a65d2a93ae80a57cc24
Nehal <nehalgarg37@gmail.com>
2023-03-29 01:33:09 +0530

    Reference vs Pointer

--- Data Structure/Pointers/Learning/PL2.cpp
+++ Data Structure/Pointers/Learning/PL2.cpp
@@ -8,6 +8,13 @@ void swap (int *x , int *y)
     *y =  temp;
 }
 
+void swapbyreference (int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
 int main()
 {
     int x = 10;
@@ -16,10 +23,11 @@ int main()
     int *p1 = &x;
     int *p2 = &y;
 
-    swap (p1,p2);
+    swap (p1,p2); // p1 and p2 are the pointer holding address. so to catch it we have to declare pointer parameter on the function.
 
     cout << x << y <<endl; 
 
-
+    swapbyreference (x,y); // here we pass the variables itself but in the function we call it by reference. Referencing means giving the another name for the same variable. Syntax : (data_type &ref = variable);
+    cout << x << y <<endl; 
     return 0;
 }
