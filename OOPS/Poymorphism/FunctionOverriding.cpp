79f3f752f7c040443322231703e2a0f52ff6cd78
Nehal <nehalgarg37@gmail.com>
2023-03-28 20:09:44 +0530

    OOPs

--- OOPS/Poymorphism/FunctionOverriding.cpp
+++ OOPS/Poymorphism/FunctionOverriding.cpp
@@ -1,7 +1,7 @@
 // Run-Time Polymorphism
 
 /*
-A virtual function is a member function which is declared within a base class and is re-defined (overridden) by a derived class. When you refer to a derived class object using a pointer or a reference to the base class, you can call a virtual function for that object and execute the derived class’s version of the function. 
+A virtual function is a member function which is declared within a base class and is re-defined (overridden) by a derived class. When you refer to a derived class object using a pointer or a reference to the base class, you can call a virtual function for that object and execute the derived class’s version of the function.
 
 1.Virtual functions ensure that the correct function is called for an object, regardless of the type of reference (or pointer) used for function call.
 2.They are mainly used to achieve Runtime polymorphism
@@ -22,8 +22,9 @@ Rules for Virtual Functions-->
 #include <iostream>
 using namespace std;
 
-class Parent{
-    public:
+class Parent
+{
+public:
     virtual void print() // using virtual keyword we do run time polymorphism. // Here child class overrides the parent class.
     {
         cout << "This is the parent class print\n";
@@ -34,8 +35,9 @@ class Parent{
     }
 };
 
-class child : public Parent{
-    public:
+class child : public Parent
+{
+public:
     void print()
     {
         cout << "This is the child class print\n";
@@ -48,19 +50,37 @@ class child : public Parent{
 
 int main()
 {
-    Parent *p,q;
-    child c;
+    Parent *p, q , *r = new Parent();
+    child c, *d = new child();
 
+    // passing address of object c (child class) to parent class object pointer.
     p = &c; // means run time pe override kr diya object variable ko.
 
     q.print();
     q.show();
 
+    cout << endl;
 
-    p->print();
-    p->show();
+    p->print(); //function overiding using virtual keyword is done.. since we refering to the base class object using parent class pointer.
+    p->show(); // here function overridding is not done. since we not use virtual keyword.
 
-    c.print();
-    c.show();
+    cout << endl;
+
+    c.print(); // function overridding done. we don't need virtual function since we are pointer to child class object using parent class pointer.
+    c.show(); // function overridding done. we don't need virtual function since we are pointer to child class object using parent class pointer.
+
+    cout << endl;
+
+    d->print(); // function overridding done. we don't need virtual function since we are pointer to child class object using parent class pointer.
+    d->show(); // function overridding done. we don't need virtual function since we are pointer to child class object using parent class pointer.
+
+    cout << endl;
+    r->print();
+    r->show();
+
+    r=&c; // passing address of object c (child class) to parent class object pointer.
+    cout << endl;
+    r->print(); //function overiding using virtual keyword is done.. since we refering to the base class object using parent class pointer.
+    r->show();  // here function overridding is not done. since we not use virtual keyword.
     return 0;
 }
