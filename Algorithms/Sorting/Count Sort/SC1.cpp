a90b4afced05b41dc89c84c1f5a5cfc8820adeb4
Nehal <nehalgarg37@gmail.com>
2023-03-30 00:10:35 +0530

    Count sort

--- Algorithms/Sorting/Count Sort/SC1.cpp
+++ Algorithms/Sorting/Count Sort/SC1.cpp
@@ -8,16 +8,20 @@ int main()
     cout << "Enter the size of the array:"<<endl;
     cin >> n;
 
+    // Declaring an array of size n.
     int arr[n];
     cout << "Enter the elements of the array:"<<endl;
 
+    // Inserting elements into array.
     for(int i=0;i<n;i++)
     {
         cin >> arr[i];
     }
 
+    // Declaring and assigning the variable temp.
     int temp = -9999;
 
+    // Finding out the largest element present in the array and storing it in the variale temp.
     for(int i=0;i<n;i++)
     {
         if(temp <= arr[i])
@@ -26,9 +30,10 @@ int main()
         }
     }
 
-
     cout << temp;
 
+    // declaring a frequency array of size [temp+1]. And intially assigning it to zero.
+    // The purpose is to count the frequency of each unique element and stores its frequency on the frequency array of index, number itself.
     int freq [temp+1] = {0};
 
     for (int i=0;i<n;i++)
@@ -45,7 +50,7 @@ int main()
     }
 
     cout << "Started storing \n";
-
+    // Iterating the loop and printing the value untill the value of the particular index not become zero.
     for(int i=0;i<=temp;i++)
     {
         while(freq[i]!=0)
