7373353fd5f642073956ed254e50daceb85f3c5c
Nehal <nehalgarg37@gmail.com>
2023-04-04 15:17:21 +0530

    recursion

---
+++ Recursion/CountTotalNumberOfPathOnAGrid.cpp
@@ -0,0 +1,34 @@
+/*
+The problem is to count all the possible paths on an m×n grid from top left (grid[0][0]) to bottom right (grid[m-1][n-1]).
+
+Having constraints that from each cell you can either move only to right or down.
+
+Input: m=2 , n=3
+Output: 3
+*/
+
+#include <iostream>
+using namespace std;
+
+int f(int i, int j,int m, int n)
+{
+    //base case
+    if(i==m-1 and j==n-1) //reach the end point.
+    {
+        return 1;
+    }
+    if(i>=m or j>=n) //went away from matrix.
+    {
+        return 0;
+    }
+
+    //recursive case.
+    return f(i,j+1,m,n) + f(i+1,j,m,n);
+}
+
+int main()
+{
+    cout<<f(0,0,3,2);
+
+    return 0;
+}
--- Recursion/PrintSumsOfAllSubsetOfArray.cpp
+++ Recursion/PrintSumsOfAllSubsetOfArray.cpp
@@ -8,10 +8,35 @@ Input: arr[] = {2,4,5}
 Output: 0 2 4 5 6 7 9 11
 */
 
-#include<iostream>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+void f(int arr[], int n, int i, int sum, vector<int> &result)
+{
+    //base case
+    if(i==n)
+    {
+        result.push_back(sum);
+        return;
+    }
+
+    //recursive case;
+    f(arr,n,i+1,sum+arr[i],result); //pick the ith element
+    f(arr,n,i+1,sum,result); //donot pick the ith element.
+}
+
+
+
 int main()
 {
-
-return 0;
+    int arr[] = {2,4,5};
+    int n=3;
+    vector<int> result;
+    f(arr, n, 0, 0,result);
+    for(int element: result)
+    {
+        cout<<element<<" ";
+    }
+    return 0;
 }
