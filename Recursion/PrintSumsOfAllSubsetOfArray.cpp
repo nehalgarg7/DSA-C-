0cb395f4701dd16b24291b587b7952f97fa30722
Nehal <nehalgarg37@gmail.com>
2023-04-04 14:26:57 +0530

    Recursion

---
+++ Recursion/PrintSumsOfAllSubsetOfArray.cpp
@@ -0,0 +1,17 @@
+// Given an array of integers, print sums of all subsets in it. Output sums can be printed in any order.
+
+/*
+Input: arr[] = {2,3}
+Output: 0 2 3 5
+
+Input: arr[] = {2,4,5}
+Output: 0 2 4 5 6 7 9 11
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+return 0;
+}
